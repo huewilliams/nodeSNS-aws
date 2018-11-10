d0bf46ebc03b43281869184b1ba34d5a36a69deb
huewilliams <4879sinwoo@naver.com>
2018-11-10 18:02:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ passportConfig(passport);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(morgan('combined'));
